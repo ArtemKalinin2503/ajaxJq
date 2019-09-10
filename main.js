9407a67a69e4199ae4f70dc218fdfbd12f6240a2
akalinin@ntdev.ru <baemohd5Oomo>
2019-09-10 14:06:17 +0300

    фильтр пользователей по цвету волос

--- main.js
+++ main.js
@@ -9,35 +9,31 @@
             success: function(res){
                 user(res.results);
             }
-        });    
+        });  
+
         //Функция вывода данных в качестве аргумента передаем ответ от ajax
         function user(data) {
-            var users = data;
-            var usersHair = [];
-            var userBrownHair = [];
-            
+            var users = data; //Данные всех пользователей
+            var usersHair = []; //Массив с цветами волос всех пользователей
+            var usersBrownHair = []; //Массив с именами пользователй у которых коричневые волосы
+            var usersFilter = []; //Массив объектов со всеми данными пользователей у которых коричневые волосы
+
             $.each(users, function(i,item){
                 $('.userBlock').append('<p>'+item.name+'</p>'); //Выведим все имена пользователей
                 usersHair.push(item.hair_color); //Запишем цвета волос пользователей в отдельный массив
             });
             
-            //Запишем в массив userBrownHair имена пользователей с коричневым цветом волос
-            for (var i = 0; i < users.length; i++) {
-                console.log(users[i]);
+            //Запишем в массив usersBrownHair имена пользователей с коричневым цветом волос
+            for (var i = 0; i < usersBrownHair.length; i++) {
                 if(users[i].hair_color == "brown") {
-                    userBrownHair.push(users[i].name);
+                    usersBrownHair.push(users[i].name);
                 }
             };
 
- 
-
-    
-
- 
-
-			// var listHeroes = this.props.heroes.results.filter(test => test.name.toLowerCase().includes(this.props.search_value)).map((item, index) => {
-			//   return <StarWarsPrew key={index} name={item.name} id={index} />;
-			// });
+            //Отфильтруем пользователей по цвету волос 
+            usersFilter = users.filter(function(currentValue, index){
+                return currentValue.hair_color == "brown" 
+            });
 
         };
 
